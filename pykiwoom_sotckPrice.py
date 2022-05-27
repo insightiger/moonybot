bafd593d484304e6fbea26b5dd87ff6362740602
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2022-05-27 19:57:19 +0900

    코스피 상장 종목 전체 가격 업데이트 완료

--- pykiwoom_sotckPrice.py
+++ pykiwoom_sotckPrice.py
@@ -1,6 +1,8 @@
+from keyword import kwlist
 from pykiwoom.kiwoom import *
 import pandas as pd
 import time
+
 
 #키움증권 open API+ 접속
 kiwoom = Kiwoom()
@@ -11,21 +13,51 @@ kospi = kiwoom.GetCodeListByMarket('0') #kospi 종목코드
 kosdaq = kiwoom.GetCodeListByMarket('10') # kosdaq 종목코드
 
 #kospi 종목이름
-kospi_name = []
-for i in kospi:
-    kospi_name.append(kiwoom.GetMasterCodeName(i))
+# kospi_name = []
+# for i in kospi:
+#     kospi_name.append(kiwoom.GetMasterCodeName(i))
 
-priceData = []
-data = kiwoom.block_request("opt10081", 종목코드="005930", 기준일자="20220521", 수정주가구분=1, output="주식일봉차트조회", next=0)
+today=time.strftime("%Y%m%d", time.gmtime(time.time()))
+print(today)
 
-print(time.localtime)
-print(data.head())
-priceData.append(data)
+priceData = {}
+priceData["date"] = ()
+
+data = kiwoom.block_request("opt10081", 종목코드=kospi[0], 기준일자=today, 수정주가구분=1, output="주식일봉차트조회", next=0)
+
+if (len(priceData["date"]) < len(data["일자"])) :
+    priceData["date"]=data["일자"]
+
+priceData[kospi[0]]=data["현재가"]
+
 
 while kiwoom.tr_remained:
-    data = kiwoom.block_request("opt10081", 종목코드="005930", 기준일자="20220521", 수정주가구분=1, output="주식일봉차트조회", next=2)
-    priceData.append(data)
-    time.sleep(1)
+    for i in kospi :
+        s = kiwoom.GetConnectState()
+        print(s, i)
+        data = kiwoom.block_request("opt10081", 종목코드=i, 기준일자=today, 수정주가구분=1, output="주식일봉차트조회", next=2)
 
-df = pd.concat(priceData)
-df.to_csv('priceData', index=False)
+        if (len(priceData["date"]) < len(data["일자"])) :
+            priceData["date"]=data["일자"]
+        
+        priceData[i]=data["현재가"]
+
+        if s == 0 :
+            print("키움증권종료됨")
+            kiwoom.CommConnect(block=True)
+            print("재로그인 완료")
+
+        time.sleep(3.6)
+
+
+df = pd.DataFrame(priceData)
+df.to_csv('kospi_price_list')
+print("done")
+
+# while kiwoom.tr_remained:
+#     data = kiwoom.block_request("opt10081", 종목코드="005930", 기준일자="20220521", 수정주가구분=1, output="주식일봉차트조회", next=2)
+#     priceData.append(data)
+#     time.sleep(1)
+
+# df = pd.concat(priceData)
+# df.to_csv('priceData', index=False)
